dfb5a0cc28a3a9d309581df5c323c7f535c7f411
Khoerul Umam <khoerulumam@students.unnes.ac.id>
2019-02-19 22:39:14 +0700

    add code

---
+++ example.php
@@ -0,0 +1,21 @@
+<?php
+
+require 'src/Knn.php';
+
+$datasample = [[7, 5, 6], [3, 4, 3], [5, 7, 7], [10, 6, 1], [12, 9, 8], [11, 1, 2]]; //Sample Data
+$datalabel = ['a', 'a', 'a', 'b', 'b', 'c']; //Label Data. [7,5,6] labeled by a, [10,6,1] labeled by b, etc
+$key = 3;     //optional. Default is 3
+$predict = [1, 7]; //Input
+
+$data = new Knn($datasample, $predict, $datalabel, $key);
+echo $data->result;
+echo "\n";
+print_r($data->getSample());
+echo "\n";
+print_r($data->getLabel());
+echo "\n";
+print_r($data->getPrediction());
+echo "\n";
+print_r($data->getSquare());
+
+
